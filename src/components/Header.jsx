2fa5566980cb602fa68e9e5807492c0fe7355303
Pablo Anjos <pablomatheussilvaanjoss@gmail.com>
2024-09-27 14:11:36 -0300

    Iniciando configuracoes nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import "../styles/header.scss"
-
+import imgMenu from "../assets/images/menu.png"
+import { useState } from "react";
 
 export function Header() {
+
+        const menuHamburguer = document.querySelector('#menu-hamburguer');
+        const buttonMenuHamburguer = document.querySelector('#menu-hamburguer-button');
+        const [active, setMode] = useState(false);
+        const toggleMode = () => {
+            setMode(!active)
+        }
+       
+        
+        
+
     return (
         <>
             <header>
                     <nav>
+                        <img src={imgMenu} alt="Imagem Menu" id="menu-hamburguer-button" />
                         <h1>Pablo Anjos</h1>
                         <ul>
                             <li><a href="#home">Home</a></li>
